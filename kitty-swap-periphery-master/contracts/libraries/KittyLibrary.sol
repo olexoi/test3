7a2e845db7d684d6fdedcba87244c7acd6ae0f05
Oleksii Poplavskyi <44132706+olexoi@users.noreply.github.com>
2020-09-26 15:22:27 +0300

    Add files via upload

--- kitty-swap-periphery-master/contracts/libraries/KittyLibrary.sol
+++ kitty-swap-periphery-master/contracts/libraries/KittyLibrary.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IKittyPair.sol';
+import './interfaces/IKittyPair.sol';
 
 import "./SafeMath.sol";
 
--- kitty-swap-periphery-master/contracts/libraries/KittyOracleLibrary.sol
+++ kitty-swap-periphery-master/contracts/libraries/KittyOracleLibrary.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IKittyPair.sol';
+import './interfaces/IKittyPair.sol';
 import '@uniswap/lib/contracts/libraries/FixedPoint.sol';
 
 // library with helper methods for oracles that are concerned with computing average prices
